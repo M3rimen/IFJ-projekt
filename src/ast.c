3be5f69e23b3704cc45f9c94c24d705717deec2a
podrazka01 <tokar.marko09@gmail.com>
2025-11-21 11:02:43 +0100

    oprava ast

--- src/ast.c
+++ src/ast.c
@@ -2,37 +2,63 @@
 #include "err.h"
 #include <stdlib.h>
 
-ASTNode *ast_new(AST_TYPE type, Token *tok) {
+ASTNode *ast_new(AST_TYPE type, Token *tok)
+{
     ASTNode *n = malloc(sizeof(ASTNode));
-    if (!n) error_exit(99, "AST malloc failed");
+    if (!n)
+        error_exit(99, "AST: malloc failed\n");
 
     n->type = type;
-    n->token = tok;
-    n->children = NULL;
     n->child_count = 0;
+    n->children = NULL;
+
+    if (tok) {
+        Token *copy = malloc(sizeof(Token));
+        if (!copy)
+            error_exit(99, "AST: malloc token failed\n");
+
+        memcpy(copy, tok, sizeof(Token));
+
+        if (tok->lexeme)
+            copy->lexeme = strdup(tok->lexeme);
+
+        n->token = copy;
+    } else {
+        n->token = NULL;
+    }
+
     return n;
 }
 
-void ast_add_child(ASTNode *parent, ASTNode *child) {
-    parent->children = realloc(parent->children,
-                               sizeof(ASTNode*) * (parent->child_count + 1));
+
+void ast_add_child(ASTNode *parent, ASTNode *child)
+{
+    parent->children = realloc(
+        parent->children,
+        sizeof(ASTNode*) * (parent->child_count + 1)
+    );
+
+    if (!parent->children)
+        error_exit(99, "AST: realloc failed\n");
 
     parent->children[parent->child_count] = child;
     parent->child_count++;
 }
 
-void ast_free(ASTNode *node) {
-    if (!node) return;
 
-    for (int i = 0; i < node->child_count; i++)
-        ast_free(node->children[i]);
+void ast_free(ASTNode *n)
+{
+    if (!n) return;
 
-    free(node->children);
+    for (size_t i = 0; i < n->child_count; i++)
+        ast_free(n->children[i]);
 
-    if (node->token) {
-        free(node->token->lexeme);
-        free(node->token);
+    free(n->children);
+
+    if (n->token) {
+        free(n->token->lexeme);
+        free(n->token);
     }
 
-    free(node);
+    free(n);
 }
