c29f725d3b991cb566db13e28bd36e22c5bfa766
M3rimen <andrej.gulas04@gmail.com>
2025-11-25 22:18:33 +0100

    opravenie err.h

--- src/err.h
+++ src/err.h
@@ -1,24 +1,27 @@
-/**
- * @file err.h
- * @author Marko Tokár (xtokarm00)
- * @brief 
- * @version 0.1
- * @date 2025-11-18
- * 
- * @copyright Copyright (c) 2025
- * 
- */
-
+// err.h
 #ifndef ERR_H
 #define ERR_H
 
-/**
- * @brief error_exit - prints formatted error message and exits program
- * 
- * @param exit_type which exit code to use
- * @param fmt string format for error message
- * @param ... 
- */
-void error_exit(int exit_type, const char *fmt, ...);
+// Kódy chýb
+typedef enum {
+    ERR_OK              = 0,   // bez chyby
+
+    ERR_LEX             = 1,   // lexikálna chyba
+    ERR_SYN             = 2,   // syntaktická chyba
+
+    ERR_SEM_UNDEF       = 3,   // nedefinovaná funkcia / premenná
+    ERR_SEM_REDEF       = 4,   // redefinícia
+    ERR_SEM_PARAM       = 5,   // zly počet/typ argumentov
+    ERR_SEM_TYPE        = 6,   // typová nekompatibilita
+    ERR_SEM_OTHER       = 10,  // ostatné sémantické
+
+    ERR_RUNTIME_UNDEF   = 25,  // behová – zlý typ parametra vstavanej funkcie
+    ERR_RUNTIME_TYPE    = 26,  // behová – typová nekompatibilita vo výrazoch
+
+    ERR_INTERNAL        = 99   // interná chyba prekladača (napr. malloc)
+} ErrorCode;
+
+// Vytlačí chybovú hlášku na stderr a ukončí program s daným kódom
+void error_exit(ErrorCode code, const char *fmt, ...);
 
 #endif // ERR_H
